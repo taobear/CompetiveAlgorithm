f64793002af2ee34971442aa39caf06342ffdaaa
taobear <1743909352@qq.com>
2020-11-23 00:47:04 +0800

    first commit

---
+++ build/BruteForce/EnumRange/simple_permutation/CMakeFiles/test_simple_permutation.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_simple_permutation.dir/test_simple_permutation.cpp.obj"
+  "CMakeFiles/test_simple_permutation.dir/simple_permutation.cpp.obj"
+  "test_simple_permutation.pdb"
+  "test_simple_permutation.exe"
+  "test_simple_permutation.exe.manifest"
+  "libtest_simple_permutation.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_simple_permutation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/Chapter06/Example/StackQueue/Exp6_1/CMakeFiles/Exp6_1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Exp6_1.dir/ConcurrencySimulator.cpp.obj"
+  "Exp6_1.pdb"
+  "Exp6_1.exe"
+  "Exp6_1.exe.manifest"
+  "libExp6_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Exp6_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
